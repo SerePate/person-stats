9e71534969ef2fce653fc89ce2e5c3828681f2ce
SerePate <serepate@umail.iu.edu>
2017-06-26 16:26:21 -0400

    handle clicks on the button and the instructions paragraph

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function changeHeading() {
-    const heading = document.querySelector('h1')
-    heading.textContent = 'Ye Olde Person Stats'
+function changeHeading(ev) {
+    
+    ev.target.textContent = 'clickety click'
 }
+
 const btn = document.querySelector('button')
-btn.addEventListener('click', changeHeading) 
+
+btn.addEventListener('click', function(){
+    changeHeading('So Fancy')}) 
+
+const p = document.querySelector('p#instructions')
+p.addEventListener('click', changeHeading)
