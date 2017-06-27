84ae290e5a7ebc1dc88ae250ca167a5d1162c1ef
SerePate <serepate@umail.iu.edu>
2017-06-27 15:34:39 -0400

    added index.css to create personalized sytle sheet

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 function changeHeading(ev) {
-    ev.preventDefault()
-    const f = ev.target
-    const name = f.personName.value
-    const heading = document.querySelector('hi')
-    heading.textContent = name
+  ev.preventDefault()
+  const f = ev.target
+  const name = f.personName.value
+
+  const h = ev.target
+  const writeColor = h.favColor.value
+
+  const heading = document.querySelector('h1')
+  heading.innerHTML = name
+
+  const paragraph = document.querySelector('div')
+  paragraph.innerHTML = name + "    " + writeColor
+
+  paragraph.style.color = writeColor; 
+  
+  
 }
 
-const personform = document.querySelector('#person-form')
-personform.addEventListener('submit', changeHeading)
+
+const personForm = document.querySelector('#person-form')
+personForm.addEventListener('submit', changeHeading)
