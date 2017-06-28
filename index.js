c51c60dcedd03a50d8a6cc5344b4ae9c8c4d778e
SerePate <serepate@umail.iu.edu>
2017-06-28 10:18:10 -0400

    renderList function is completed. Reorganized the code to make sense.

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-<<<<<<< HEAD
-function renderColor(color, colorItem){
+function renderColor(color){
 
   const colorDiv = document.createElement('div')
   colorDiv.style.backgroundColor = color
   colorDiv.style.width = '6rem'
   colorDiv.style.height = '3rem'
-  colorItem.appendChild(colorDiv)
-
+  return colorDiv
 }
-function renderListItem(nameItem, ageItem, age, name){
+
+function renderListItem(age, name, nameItem, ageItem){
 
   const nameList = document.createElement('li')
-  nameList.textContent = `Name: ${name}`
+  ageList.textContent = `Name: ${name}`
   nameItem.appendChild(nameList)
+ 
 
   const ageList = document.createElement('li')
   ageList.textContent = `Age: ${age}`
   ageItem.appendChild(ageList)
-}
-
-function renderList(){
   
 }
 
-function handleSubmit(ev) {
-  ev.preventDefault()
+function renderList(ev){
   const f = ev.target
   const name = f.personName.value
   const age = f.personAge.value
   const color = f.favoriteColor.value
 
-  const div = document.querySelector('#stats')
-  
   const list = document.createElement('ul')
+
+  const div = document.querySelector('#stats')
 
   const nameItem = document.createElement('li')
   nameItem.textContent = `Name: ${name}`
- // list.appendChild(nameItem)
+  list.appendChild(nameItem)
+ 
 
   const ageItem = document.createElement('li')
   ageItem.textContent = `Age: ${age}`
-  //list.appendChild(ageItem)
-
-  const colorItem = document.createElement('li')
-  colorItem.textContent = 'Favorite Color: '
-
-  renderColor(color,colorItem)
-  renderListItem(nameItem, ageItem)
-
-  list.appendChild(nameItem)
-  
   list.appendChild(ageItem)
 
+
+  const colorItem = document.createElement('li')
+  const colorDiv = renderColor(color)
+  colorItem.textContent = 'Favorite Color: '
+  colorItem.appendChild(colorDiv)
   list.appendChild(colorItem)
 
   div.appendChild(list)
 
-  // div.innerHTML = '<p>' + name + ', age ' + age + '</p>'
-  // div.innerHTML = `<p style="background-color: ${color}">${name}, age ${age}</p>`
+  // renderColor(color)
+  // renderListItem(nameItem, ageItem)
+
+}
+
+function handleSubmit(ev) {
+  ev.preventDefault()
+  
+
+  renderList(ev)
 }
 
 
