d56e00f9ec7df778454bf2867200ba57e21f5459
Serena Patel <serena_patel@outlook.com>
2017-06-27 21:51:30 -0400

    Add files via upload
    renderListItem and renderColor functions created and implemented.

--- index.js
+++ index.js
@@ -1,22 +1,59 @@
-function changeHeading(ev) {
+function renderColor(color, colorItem){
+
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = color
+  colorDiv.style.width = '6rem'
+  colorDiv.style.height = '3rem'
+  colorItem.appendChild(colorDiv)
+
+}
+function renderListItem(nameItem, ageItem, age, name){
+
+  const nameList = document.createElement('li')
+  nameList.textContent = `Name: ${name}`
+  nameItem.appendChild(nameList)
+
+  const ageList = document.createElement('li')
+  ageList.textContent = `Age: ${age}`
+  ageItem.appendChild(ageList)
+}
+
+function handleSubmit(ev) {
   ev.preventDefault()
   const f = ev.target
   const name = f.personName.value
+  const age = f.personAge.value
+  const color = f.favoriteColor.value
 
-  const h = ev.target
-  const writeColor = h.favColor.value
-
-  const heading = document.querySelector('h1')
-  heading.innerHTML = name
-
-  const paragraph = document.querySelector('div')
-  paragraph.innerHTML = name + "    " + writeColor
-
-  paragraph.style.color = writeColor; 
+  const div = document.querySelector('#stats')
   
-  
+  const list = document.createElement('ul')
+
+  const nameItem = document.createElement('li')
+  nameItem.textContent = `Name: ${name}`
+ // list.appendChild(nameItem)
+
+  const ageItem = document.createElement('li')
+  ageItem.textContent = `Age: ${age}`
+  //list.appendChild(ageItem)
+
+  const colorItem = document.createElement('li')
+  colorItem.textContent = 'Favorite Color: '
+
+  renderColor(color,colorItem)
+  renderListItem(nameItem, ageItem)
+
+  list.appendChild(nameItem)
+  list.appendChild(ageItem)
+
+  list.appendChild(colorItem)
+
+  div.appendChild(list)
+
+  // div.innerHTML = '<p>' + name + ', age ' + age + '</p>'
+  // div.innerHTML = `<p style="background-color: ${color}">${name}, age ${age}</p>`
 }
 
 
 const personForm = document.querySelector('#person-form')
-personForm.addEventListener('submit', changeHeading)
+personForm.addEventListener('submit', handleSubmit)
